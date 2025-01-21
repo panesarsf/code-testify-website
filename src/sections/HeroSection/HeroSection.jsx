5122a0dfa5cf0c70fe47618ae869d4fc750aa3aa
gurpreet <panesarsf@gmail.com>
2025-01-21 20:12:53 +0530

    updated by GS

--- src/sections/HeroSection/HeroSection.jsx
+++ src/sections/HeroSection/HeroSection.jsx
@@ -10,6 +10,41 @@ const HeroSection = () => {
         Your Trusted Partner for Quality Assurance, Software Automation, and
         Custom Solutions
       </p>
+      <h3>What We Do</h3>
+      <p>
+        From consultation, creation, promotion & automation, get everything your business needs
+        to leap into the next phase of growth.
+      </p>
+
+      {/* Our Clients */}
+      <h3>Our Clients</h3>
+      <p>
+        Our portfolio holds the work of some of the most reputed enterprise & startup brands.
+        All it is missing is YOU!
+      </p>
+
+      {/* Our Success Stories */}
+      <h3>Our Success Stories</h3>
+      <p>
+        Our portfolio holds the work of some of the most reputed enterprise & startup brands.
+        All it is missing is YOU!
+      </p>
+      <button className={styles.learnMoreButton}>Read Full Case Study</button>
+
+      {/* Client Testimonial */}
+      <h3>Client Testimonial</h3>
+      <p>
+        1. Our portfolio holds the work of some of the most reputed enterprise & startup brands.
+        All it is missing is YOU!
+      </p>
+      <p>
+        2. Our portfolio holds the work of some of the most reputed enterprise & startup brands.
+        All it is missing is YOU!
+      </p>
+      <p>
+        3. Our portfolio holds the work of some of the most reputed enterprise & startup brands.
+        All it is missing is YOU!
+      </p>
       <button className={styles.learnMoreButton}>Learn More</button>
     </div>
   );
